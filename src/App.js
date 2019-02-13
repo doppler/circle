ecd90eb787a313c994ce2ae116dbe27ccf7f10f6
David Rose <doppler@gmail.com>
2019-02-12 19:08:20 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const App = () => {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const handleXChange = event => setX(Number(event.target.value));
+  const handleYChange = event => setY(Number(event.target.value));
+  return (
+    <div className="App">
+      <div className="circle-container">
+        <div className="inner-box" />
+        <div className="inner-box" />
+        <div className="inner-box" />
+        <div className="inner-box" />
+        <div
+          className="dot"
+          style={{
+            left: `calc(50% - 5pt + ${x}pt)`,
+            top: `calc(50% - 5pt + ${y}pt)`
+          }}
+        />
       </div>
-    );
-  }
-}
+      <SliderX {...{ x, handleXChange }} />
+      <SliderY {...{ y, handleYChange }} />
+    </div>
+  );
+};
 
 export default App;
+
+const SliderX = ({ x, handleXChange }) => (
+  <input type="range" min={-200} max={200} value={x} onChange={handleXChange} />
+);
+
+const SliderY = ({ y, handleYChange }) => (
+  <input type="range" min={-200} max={200} value={y} onChange={handleYChange} />
+);
